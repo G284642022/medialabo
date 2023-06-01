92f9abaaf73e6bad799133bc67510c6a02e5f76e
G284642022 <228464@st.takushoku-u.ac.jp>
2023-06-01 16:57:55 +0900

    課題

--- kazuate.js
+++ kazuate.js
@@ -9,16 +9,16 @@ let sei = 0;
 // 予想を4回実行する 
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-let b = document.querySelector('button#poti');
+let b = document.querySelector('#poti');
 b.addEventListener('click',hantei );
 
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  let i = document.querySelector('input[name="yosou"]');
-    yoso = i.Number(); 
+  let n = document.querySelector('input[name="yosou"]'); 
+  let yoso = parseInt(n.value);
+  
   kaisu = kaisu + 1;
   let kai = document.querySelector('span#kaisu');
   kai = kaisu;
