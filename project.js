2298a16315047bc06bafd149918146a5fabc9d4d
G284642022 <228464@st.takushoku-u.ac.jp>
2023-06-07 02:25:08 +0900

    課題

--- project.js
+++ project.js
@@ -1,54 +1,40 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+let ten = document.querySelector('#poti');
+ten.addEventListener('click',sendRequest);
 
-////////// 課題3-2 ここからプログラムを書こう
-let n = document.querySelector('table');
-  an = data.name;
-  
-console.log(data.name);
-console.log(data.main.temp_max);
-console.log(data.main.temp_min);
+
+// 通信を開始する処理
+function sendRequest() {
+    // URL を設定
+    let a = document.querySelector('input[name="weather"]');
+    let id = parseInt(a.value);
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json';
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    let ken = document.querySelector('p#kensaku');
+    kensaku.textContent = data.name+'の天気は'+data.weather.description+'です。'+ '最高気温は'+data.main.temp_max+'°です。最低気温は'+data.main.temp_min+'°です。'
+}
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+}
